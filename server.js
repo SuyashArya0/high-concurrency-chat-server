b38ab468615975e36e52b7a64f1e0612e1c8a9b7
SuyashArya0 <aryasuyash55@gmail.com>
2026-08-27 17:48:10 +0530

    feat: add server entry point to instantiate and start the Fastify server

---
+++ server.js
@@ -0,0 +1,23 @@
+const buildApp = require('./src/app');
+
+async function start()
+{
+    const app = await buildApp({
+        logger: true
+    });
+
+    try
+    {
+        await app.listen({
+            port: app.config.PORT,
+            host: app.config.HOST
+        });
+    }
+    catch(err)
+    {
+        app.log.error(err);
+        process.exit(1);
+    }
+}
+
+start();
